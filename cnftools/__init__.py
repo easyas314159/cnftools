e98fe21dd5ea5c2966d2c866f87bdf153f35b5ee
Kevin Loney <kevin.loney@brainsinjars.com>
2018-12-01 16:13:03 -0700

    initial commit of k-coloring (chromatic number) tool

--- cnftools/__init__.py
+++ cnftools/__init__.py
@@ -1,4 +1,5 @@
+from .coloring import color
 from .tseytin import to_3cnf
 from .io import DimacsException, load, dump
 
-__all__ = ['DimacsException', 'load', 'dump', 'to_3cnf']
+__all__ = ['DimacsException', 'load', 'dump', 'to_3cnf', 'color']
---
+++ cnftools/coloring.py
@@ -0,0 +1,16 @@
+import argparse
+
+from itertools import combinations
+
+def color(graph, chromatic_number):
+	literals = {}
+	for node in graph.nodes():
+		index = chromatic_number * node + 1
+		literals[node] = list(range(index, index + chromatic_number))
+
+	for node in graph.nodes():
+		yield literals[node]
+		yield from [[-u, -v] for u, v in combinations(literals[node], r=2)]
+
+	for u, v in graph.edges():
+		yield from [[-l0, -l1] for l0, l1 in zip(literals[u], literals[v])]
