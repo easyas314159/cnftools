57aac9114b623029a40b5596d950a359d3a0c8f0
Kevin Loney <kevin.loney@brainsinjars.com>
2018-12-01 17:56:37 -0700

    Added entry point for the kcoloring tool

--- setup.py
+++ setup.py
@@ -43,6 +43,7 @@ setuptools.setup(
 	entry_points={
 		'console_scripts': [
 			'to3cnf = cnftools.cmdline.to3cnf:main',
+			'kcoloring = cnftools.cmdline.kcoloring:main',
 		],
 	},
 	install_requires=[
