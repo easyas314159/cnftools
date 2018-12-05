53cc633ce4843c18413189f892bddc7ab3ca5b0a
Kevin Loney <kevin.loney@brainsinjars.com>
2018-12-04 22:22:26 -0700

    Refactored simplify implementation so it provides extra tools

--- cnftools/simplify.py
+++ cnftools/simplify.py
@@ -1,47 +1,62 @@
-def simplify(clauses):
-	cnf_1, cnf_n = set(), set()
-	for original in clauses:
-		# Make sure to simplify (L | ... | L) to (L)
-		clause = frozenset(original)
+def clean(clauses):
+	for clause in clauses:
+		# Simplify (L | ... | L) to (L)
+		clause = set(clause)
 
 		# If clause contains (L | -L) then it is always true so we can skip it
 		if len(clause) != len(set((abs(l) for l in clause))):
 			continue
 
-		# Remove duplicate clauses
-		if (len(clause) == 1):
+		yield clause
+
+def unit_propagate(clauses, cascade=True):
+	cnf_1, cnf_n = set(), set()
+	cnf_other = list()
+
+	for clause in clauses:
+		if len(clause) == 1:
 			cnf_1.add(list(clause)[0])
 		else:
-			cnf_n.add(clause)
+			cnf_other.append(clause)
+
+	if not cnf_1:
+		return cnf_1, cnf_other
+
+	for clause in cnf_other:
+		clause = set(clause)
+		for l in list(clause):
+			# Remove clauses containing True literals
+			if l in cnf_1:
+				break
+			# Remove false literals from clauses
+			if -l in cnf_1:
+				clause.remove(l)
+		else:
+			cnf_n.add(frozenset(clause))
+
+	if cascade:
+		cnf_1_next, cnf_n = unit_propagate(cnf_n)
+		cnf_1.update(cnf_1_next)
+
+	return cnf_1, cnf_n
+
+def simplify(clauses):
+	# Remove duplicate clauses
+	cnf_n = set((frozenset(c) for c in clean(clauses)))
+	cnf_1 = set()
 
 	modified = True
 	while modified:
-		cnf_next = set()
 		modified = False
-		for clause in cnf_n:
-			clause = set(clause)
-			for l in list(clause):
-				# Remove clauses containing True literals
-				if l in cnf_1:
-					modified = True
-					break
 
-				# Remove false literals from clauses
-				if -l in cnf_1:
-					modified = True
-					clause.remove(l)
-			else:
-				cnf_next.add(frozenset(clause))
+		cnf_1_new, cnf_n = unit_propagate(cnf_n)
+		cnf_1.update(cnf_1_new)
+
+		if cnf_1_new:
+			modified = True
 
 		# TODO: Solve for implied literals
 		# TODO: Remove subsumed clauses (e.g. (a|b) => (a|b|c))
-
-		cnf_n = set()
-		for clause in cnf_next:
-			if (len(clause) == 1):
-				cnf_1.add(list(clause)[0])
-			else:
-				cnf_n.add(clause)
 
 	yield from (set([l]) for l in cnf_1)
 	yield from cnf_n
