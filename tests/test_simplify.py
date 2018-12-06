c52a114f5c98c621f4d86a1ee37bd9240808dd46
Kevin Loney <kevin.loney@brainsinjars.com>
2018-12-06 11:28:18 -0700

    Added tests for `simplify.unit_propagate`

--- tests/test_simplify.py
+++ tests/test_simplify.py
@@ -23,3 +23,33 @@ class CleanTests(unittest.TestCase):
 		cleaned = make_comparable(cnftools.clean(clauses))
 
 		self.assertSetEqual(cleaned, make_comparable([[1, 2]]))
+
+class UnitPropagateTests(unittest.TestCase):
+	def test_single(self):
+		clauses = [[-1], [2]]
+		cnf_1, cnf_n = cnftools.unit_propagate(clauses)
+
+		self.assertSetEqual(cnf_1, set([-1, 2]))
+		self.assertListEqual(cnf_n, [])
+
+	def test_cascade(self):
+		clauses = [[-1], [-1, 2], [1, 2]]
+		cnf_1, cnf_n = cnftools.unit_propagate(clauses)
+
+		self.assertSetEqual(cnf_1, set([-1, 2]))
+		self.assertListEqual(cnf_n, [])
+
+	def test_contradiction(self):
+		clauses = [[-1], [2], [1, -2]]
+
+		cnf_1, cnf_n = cnftools.unit_propagate(clauses)
+
+		self.assertSetEqual(cnf_1, set([-1, 2]))
+		self.assertSetEqual(make_comparable(cnf_n), make_comparable([[]]))
+
+	def test_trailing(self):
+		clauses = [[3, 4]]
+		cnf_1, cnf_n = cnftools.unit_propagate(clauses)
+
+		self.assertSetEqual(cnf_1, set())
+		self.assertSetEqual(make_comparable(cnf_n), make_comparable([[3, 4]]))
