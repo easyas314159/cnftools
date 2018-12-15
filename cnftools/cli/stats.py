b360312fa9ab808066f917cebf03d1c29bb58990
Kevin Loney <kevin.loney@brainsinjars.com>
2018-12-15 11:20:30 -0700

    Fixed a big in `cnf stats ...` that was introduced in d107a49

--- cnftools/cli/stats.py
+++ cnftools/cli/stats.py
@@ -61,8 +61,8 @@ def main(args):
 		for cluster in clusters.values():
 			clustergram[len(cluster)] += 1
 
-		for length in sorted(clustergram.keys()):
+		for nclauses in sorted(clustergram.keys()):
 			print('\t{nclauses:d}: {nclusters:d}'.format(
-				nclauses=length,
-				nclusters=clustergram[length]
+				nclauses=nclauses,
+				nclusters=clustergram[nclauses]
 			))
